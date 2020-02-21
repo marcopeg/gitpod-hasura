ae6654a0856d0a669c96fa6c3b0ea79c46eb7850
Marco Pegoraro <marco.pegoraro@gmail.com>
2020-02-21 14:59:38 +0000

    Boot script

--- hasura-start.sh
+++ hasura-start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-HASURA_GRAPHQL_DATABASE_URL=postgres://localhost:5432/postgres
-HASURA_GRAPHQL_ENABLE_CONSOLE=true
-HASURA_GRAPHQL_ADMIN_SECRET=${HASURA_GRAPHQL_ADMIN_SECRET-qwerty}
+export HASURA_GRAPHQL_DATABASE_URL=postgres://localhost:5432/postgres
+export HASURA_GRAPHQL_ENABLE_CONSOLE=true
+export HASURA_GRAPHQL_ADMIN_SECRET=${HASURA_GRAPHQL_ADMIN_SECRET:-qwerty}
 
 /bin/graphql-engine serve
